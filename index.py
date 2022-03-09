62cbe4684d14f12a800b91abac5b2c4c3fcc0a08
Aliandsoopdogg <muhammederaliev973@gmail.com>
2022-03-09 18:53:03 +0600

    naroto

---
+++ index.py
@@ -0,0 +1,100 @@
+import csv
+import json
+import os
+import time
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+def get_pages():
+    headers = {
+        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"
+    }
+
+    # u = requests.get(url='https://shop.casio.ru/catalog/g-shock/filter/gender-is-male/apply/', headers=headers)
+
+    # if not os.path.exists("data"):
+    #     os.mkdir("home_work/data")
+
+    # with open("home_work/data/page_1.html", "w") as file:
+    #     file.write(u.text)
+
+    with open("home_work/data/page_1.html") as file:
+        src = file.read()
+    
+    soup = BeautifulSoup(src, "lxml")
+    pages_count = int(soup.find("div", class_="bx-pagination-container").find_all("a")[-2].text)
+    
+    for i in range(1, pages_count + 1):
+        url = f"https://shop.casio.ru/catalog/g-shock/filter/gender-is-male/apply/?PAGEN_1={i}"
+        
+        u = requests.get(url=url, headers=headers)
+
+        with open(f"home_work/data/page_{i}.html", "w") as file:
+            file.write(u.text)
+
+        time.sleep(2)
+
+    return pages_count + 1
+
+
+def collect_data(pages_count):
+    cur_date = datetime.now().strftime("%d_%m_%Y")
+
+    with open(f"home_work/data_{cur_date}.csv", "w") as file:
+        writer = csv.writer(file)
+
+        writer.writerow(
+            (
+                "Артикул",
+                "Ссылка",
+                "Цена"
+            )
+        )
+
+    data = []
+    for page in range(1, pages_count):
+        with open(f"home_work/data/page_{page}.html") as file:
+            src = file.read()
+
+        soup = BeautifulSoup(src, "lxml")
+        items_cards = soup.find_all("a", class_="product-item__link")
+
+        for item in items_cards:
+            product_article = item.find("p", class_="product-item__articul").text.strip()
+            product_price = item.find("p", class_="product-item__price").text.lstrip("руб. ")
+            product_url = f'https://shop.casio.ru{item.get("href")}'
+
+            
+
+            data.append(
+                {
+                    "product_article": product_article,
+                    "product_url": product_url,
+                    "product_price": product_price
+                }
+            )
+
+            with open(f"home_work/data_{cur_date}.csv", "a") as file:
+                writer = csv.writer(file)
+
+                writer.writerow(
+                    (
+                        product_article,
+                        product_url,
+                        product_price
+                    )
+                )
+
+        print(f"[INFO] Обработана страница {page}/5")
+
+    with open(f"home_work/data_{cur_date}.json", "a") as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
+
+
+def main():
+    pages_count = get_pages()
+    collect_data(pages_count=pages_count)
+
+if __name__ == '__main__':
+    main()
